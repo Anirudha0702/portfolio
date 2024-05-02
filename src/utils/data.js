1a34a134f164b14f18536f693550f3fc6bc0a4c5
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2024-05-02 15:07:28 +0530

    link-reume --update

--- src/utils/data.js
+++ src/utils/data.js
@@ -71,30 +71,6 @@ export const skills = [
   ]
   export const projects=[
     {
-      title:'Weather App',
-      description:'It is an weather app which shows weather of current location by default and weather of other cities by searching. It also gives 1 day forecast of the city. Beside that It also gives links of few climate related news articles.It uses OpenWeather API for fetching weather data.',
-      techStack:['React','Tailwind CSS','TypeScript'],
-      github:'https://github.com/Anirudha0702/weather-app',
-      live:'https://weatheria130.netlify.app/',
-      image:'https://firebasestorage.googleapis.com/v0/b/portfolio-cb13b.appspot.com/o/project_view%2FScreenshot%202024-04-02%20020009.png?alt=media&token=0fa9c4d1-e163-44ac-9931-fe0852fa9b19'
-    },
-    {
-      title:'UnityChat',
-      description:'A web chat application with a global chat  room  for all users. User need to sign in with their google account to chat. It uses Firebase for authentication and firestore for storing messages,and uses firestore ,mongoDB for storing user data.It also uses firestore real time update feature to show messages in real time.',
-      techStack:['React','Firebase','Tailwind','MongoDB'],
-      github:'https://github.com/Anirudha0702/UnityChat/tree/update-feature',
-      live:'https://unitychat.netlify.app/',
-      image:'https://firebasestorage.googleapis.com/v0/b/portfolio-cb13b.appspot.com/o/project_view%2FScreenshot%202024-04-02%20015918.png?alt=media&token=d5126fe7-591a-4d4e-a35a-49b515c6d1ff'
-    },
-    {
-      title:'Portfolio',
-      description:"My Personal Portfolio contains my latest projects and creative endeavors. It showcases my skills and expertise.",
-      techStack:['React','css'],
-      github:'https://github.com/Anirudha0702/portfolio/tree/social-media-UI',
-      live:'https://anirudha0702.netlify.app/',
-      image:'https://firebasestorage.googleapis.com/v0/b/portfolio-cb13b.appspot.com/o/project_view%2FScreenshot%202024-04-02%20015949.png?alt=media&token=d1a5d6ce-4e3c-44a4-ad3d-70ce05fdf5ba'
-    },
-    {
       title:'Scrap Go-Go',
       description:"This project is a web scraper tool build with express Js and typescript. It scraps gogoAnime website.This tool includes 3 features Search Anime by Keyword, Get Anime Information, Get Streaming links of Episodes",
       techStack:['Express Js','TypeScript','HTML','CSS'],
@@ -109,7 +85,32 @@ export const skills = [
       github:'https://github.com/Anirudha0702/weeb-e-fied',
       live:'https://weeb-e-fied.netlify.app/',
       image:'https://firebasestorage.googleapis.com/v0/b/portfolio-cb13b.appspot.com/o/project_view%2Fweeb-e-fied.png?alt=media&token=9796e403-7535-4aa2-8265-bd20e8f16c5c'
-    }
+    },
+    {
+      title:'Weather App',
+      description:'It is an weather app which shows weather of current location by default and weather of other cities by searching. It also gives 1 day forecast of the city. Beside that It also gives links of few climate related news articles.It uses OpenWeather API for fetching weather data.',
+      techStack:['React','Tailwind CSS','TypeScript'],
+      github:'https://github.com/Anirudha0702/weather-app',
+      live:'https://weatheria130.netlify.app/',
+      image:'https://firebasestorage.googleapis.com/v0/b/portfolio-cb13b.appspot.com/o/project_view%2FScreenshot%202024-04-02%20020009.png?alt=media&token=0fa9c4d1-e163-44ac-9931-fe0852fa9b19'
+    },
+    {
+      title:'UnityChat',
+      description:'A web chat application with a global chat  room  for all users. User need to sign in with their google account to chat. It uses Firebase for authentication and firestore for storing messages,and uses firestore for storing user data.It also uses firestore real time update feature to show messages in real time.',
+      techStack:['React','Firebase','Tailwind'],
+      github:'https://github.com/Anirudha0702/UnityChat/tree/update-feature',
+      live:'https://unitychat.netlify.app/',
+      image:'https://firebasestorage.googleapis.com/v0/b/portfolio-cb13b.appspot.com/o/project_view%2FScreenshot%202024-04-02%20015918.png?alt=media&token=d5126fe7-591a-4d4e-a35a-49b515c6d1ff'
+    },
+    {
+      title:'Portfolio',
+      description:"My Personal Portfolio contains my latest projects and creative endeavors. It showcases my skills and expertise.",
+      techStack:['React','css'],
+      github:'https://github.com/Anirudha0702/portfolio/tree/social-media-UI',
+      live:'https://anirudha0702.netlify.app/',
+      image:'https://firebasestorage.googleapis.com/v0/b/portfolio-cb13b.appspot.com/o/project_view%2FScreenshot%202024-04-02%20015949.png?alt=media&token=d1a5d6ce-4e3c-44a4-ad3d-70ce05fdf5ba'
+    },
+    
     // https://firebasestorage.googleapis.com/v0/b/portfolio-cb13b.appspot.com/o/project_view%2Fweeb-e-fied.png?alt=media&token=9796e403-7535-4aa2-8265-bd20e8f16c5c
   ]
-  export const Resume='https://firebasestorage.googleapis.com/v0/b/portfolio-cb13b.appspot.com/o/myResune.pdf?alt=media&token=2e370585-e124-4349-9118-491b7f3de7f7'
+  export const Resume='https://firebasestorage.googleapis.com/v0/b/portfolio-cb13b.appspot.com/o/myResune.pdf?alt=media&token=53171cad-be1f-4511-ad11-24b3099d44cf'
